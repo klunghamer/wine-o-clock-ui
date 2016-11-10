bf229ad8c329093b29269fa9c0f00813d656ba73
Kelly Lunghamer <klung@umich.edu>
2016-11-10 17:25:37 -0500

    Starting frontend

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var app = express();
+var port = process.env.PORT || 4000;
+
+// MIDDLEWARE/CONFIGS
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use('/scripts', express.static(__dirname + '/bower_components'))
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.all('/*', function(req, res, next) {
+  res.sendFile('/public/index.html', { root: __dirname });
+});
+
+app.listen(port, function() {
+  console.log('======================');
+  console.log('listening on port 4000');
+  console.log('======================');
+});
